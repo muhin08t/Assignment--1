b28040ad26ea7abb406c2873579c754480402e5c
Mohiuddin <muhin08t@gmail.com>
2024-08-03 17:49:03 +0600

    Result function added

--- main.js
+++ main.js
@@ -1,41 +1,51 @@
-let totalValue;
+let pinValue;
+let secretKey;
 
 function generateSecretKey() {
-    let text =  Math.floor(100000 + Math.random() * 900000);
-    document.getElementById("btn_display_key").innerHTML = text;
+    secretKey =  Math.floor(100000 + Math.random() * 900000);
+    document.getElementById("btn_display_key").innerHTML = secretKey;
 }
 
 function displayResult() {
     let x = document.getElementById("pin").value;
     console.log("value "+x);
+    if(x && secretKey && x==secretKey) {
+            console.log("value equal ");
+            document.getElementById("success-msg").style.visibility = "visible";
+            document.getElementById("wrong-msg").style.visibility = "hidden";
+    } else {
+        console.log("value are not equal ");
+        document.getElementById("success-msg").style.visibility = "hidden";
+        document.getElementById("wrong-msg").style.visibility = "visible";
+    }
 }
 function processInput(val){
     let x = document.getElementById("pin").value;
-    if(totalValue){
-        totalValue = totalValue+val;
+    if(pinValue){
+        pinValue = pinValue+val;
     } else {
-        totalValue = val;
+        pinValue = val;
     }
     if(x){
-        totalValue = x + val;
+        pinValue = x + val;
     }
   
      console.log(val);
-     console.log(totalValue);
-     document.getElementById("pin").value = totalValue;
+     console.log(pinValue);
+     document.getElementById("pin").value = pinValue;
 }
 
 function resetInput() {
     document.getElementById("pin").value = "";
-    totalValue = "";
+    pinValue = "";
 }
 
 function deleteSingleInput() {
-    totalValue = document.getElementById("pin").value;
-    if(totalValue) {
-        totalValue.toString();
-        let x = totalValue.slice(0,-1);
-        totalValue = x;
-        document.getElementById("pin").value = totalValue;
+    pinValue = document.getElementById("pin").value;
+    if(pinValue) {
+        pinValue.toString();
+        let x = pinValue.slice(0,-1);
+        pinValue = x;
+        document.getElementById("pin").value = pinValue;
     }
 }
